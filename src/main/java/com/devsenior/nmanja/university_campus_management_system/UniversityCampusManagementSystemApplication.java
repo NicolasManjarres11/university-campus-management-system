0f40d03fa295f7539b2a8eb17c274391e1ab4a98
Nicolas Manjarres Gonzalez <nicolasmanjarresg@gmail.com>
2025-06-26 19:09:38 -0500

    first commit

---
+++ src/main/java/com/devsenior/nmanja/university_campus_management_system/UniversityCampusManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.devsenior.nmanja.university_campus_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityCampusManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityCampusManagementSystemApplication.class, args);
+	}
+
+}
