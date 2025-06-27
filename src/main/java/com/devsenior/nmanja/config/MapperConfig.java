01ed04d530698f7d6e52c0e2d9d16e0fd4671086
Nicolas Manjarres Gonzalez <nicolasmanjarresg@gmail.com>
2025-06-27 05:30:26 -0500

    probando aplicacion sin los mappers, se evidencia que si arranca

---
+++ src/main/java/com/devsenior/nmanja/config/MapperConfig.java
@@ -0,0 +1,17 @@
+package com.devsenior.nmanja.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/* import com.devsenior.nmanja.mappers.StudentMapper;
+import com.devsenior.nmanja.mappers.StudentMapperImpl; */
+
+@Configuration
+public class MapperConfig {
+
+/*     @Bean
+    public StudentMapper studentMapper(){
+        return new StudentMapperImpl();
+    } */
+    
+}
---
+++ src/main/java/com/devsenior/nmanja/controllers/CampusController.java
@@ -0,0 +1,30 @@
+package com.devsenior.nmanja.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsenior.nmanja.university_campus_management_system.model.dto.StudentResponse;
+/* import com.devsenior.nmanja.university_campus_management_system.services.StudentService; */
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/campus")
+/* @RequiredArgsConstructor */
+public class CampusController {
+
+/*     private final StudentService studentService; */
+
+/*     @GetMapping("/students")
+    public List<StudentResponse> getAllStudents() {
+        return studentService.getAllStudents();
+    } */
+    
+    
+}
